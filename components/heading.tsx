508ebb2c17601a57873c80dfb3b4dc7fb1874e1a
0xKeccak <remiemeriaupro@gmail.com>
2025-07-25 00:06:20 +0200

    Fixing errors

--- components/heading.tsx
+++ components/heading.tsx
@@ -14,9 +14,9 @@ export const Heading = ({
   as?: any;
   children: any;
   size?: "sm" | "md" | "xl" | "2xl";
-  props?: React.HTMLAttributes<HTMLHeadingElement | AnimationProps>;
+  props?: React.HTMLAttributes<HTMLHeadingElement>;
 } & MotionProps &
-  React.HTMLAttributes<HTMLHeadingElement | AnimationProps>) => {
+  React.HTMLAttributes<HTMLHeadingElement>) => {
   const sizeVariants = {
     sm: "text-xl md:text-2xl md:leading-snug",
     md: "text-3xl md:text-5xl md:leading-tight",
